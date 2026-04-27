2a57228a5ee5c3308548839c606342724fecff22
thififinha <thiffanykoster2008@gmail.com>
2026-04-27 17:13:47 -0300

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <img src="https://cdn.prod.website-files.com/6752dfe21545490d913c0292/6753784a595a16591c0099e8_System-Of-A-Down.png" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ pages/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Bem-vindo ao site sobre System of a Down!</h2>
+      <p>Explore informações sobre a banda, músicas e muito mais.</p>
+    </div>
+  );
+}
+
+export default Home;
